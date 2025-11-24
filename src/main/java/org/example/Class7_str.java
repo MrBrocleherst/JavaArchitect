c1783130183e38c35d6cb75dcd945e795ee31b0d
macbook <edemseitasanov@gmail.com>
2025-11-24 17:51:07 +0200

    Courses JavaArchitect_25

--- src/main/java/org/example/Class7_str.java
+++ src/main/java/org/example/Class7_str.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 public class Class7_str {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a symbol: ");
+        char c = scanner.next().charAt(0);
         String str = scanner.nextLine();
         boolean x = str.contains(". ");
         boolean y = str.contains("! ");
         boolean z = str.contains("? ");
+        boolean a = c >= '0';
+        boolean b = c <= '9';
+        boolean cond1 = a || b; // true || false = true
 
-        if (x) {
+        if (cond1) {
             System.out.println("There is a symvols");
         } else if (y) {
             System.out.println("There is a symvols");
